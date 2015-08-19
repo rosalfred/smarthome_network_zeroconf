461afee667a918c86c86b72c8b43d5c3263f1683
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2015-08-19 19:22:01 +0200

    Update headers

--- src/main/java/com/alfred/ros/zeroconf/DiscoveredService.java
+++ src/main/java/com/alfred/ros/zeroconf/DiscoveredService.java
@@ -1,7 +1,20 @@
+/**
+ * This file is part of the Alfred package.
+ *
+ * (c) Mickael Gaillard <mick.gaillard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 package com.alfred.ros.zeroconf;
 
 import java.util.HashMap;
 
+/**
+ *
+ * @author Erwan Le Huitouze <erwan.lehuitouze@gmail.com>
+ *
+ */
 public class DiscoveredService extends com.github.rosjava.zeroconf_jmdns_suite.jmdns.DiscoveredService {
     public HashMap<String, String> properties = new HashMap<String, String>();
 }
--- src/main/java/com/alfred/ros/zeroconf/NodeConfiguration.java
+++ src/main/java/com/alfred/ros/zeroconf/NodeConfiguration.java
@@ -1,9 +1,22 @@
+/**
+ * This file is part of the Alfred package.
+ *
+ * (c) Mickael Gaillard <mick.gaillard@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 package com.alfred.ros.zeroconf;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ *
+ * @author Erwan Le Huitouze <erwan.lehuitouze@gmail.com>
+ *
+ */
 public class NodeConfiguration {
     private static final String MASTER_ADDRESS = "MASTER_ADDRESS";
     private static final String NODE_TYPE = "NODE_TYPE";
@@ -11,38 +24,38 @@ public class NodeConfiguration {
     private static final String NODE_PERMISSION_NAME = "NODE_PERMISSION_NAME";
     private static final String NODE_PERMISSION_EXCLUDE = "NODE_PERMISSION_EXCLUDE";
     private static final String NODE_CAPABILITY = "NODE_CAPABILITY";
-    
+
     private String nodeType;
     private String nodePath;
     private String masterAddress;
-    
+
     private List<NodePermission> permissions =
             new ArrayList<NodeConfiguration.NodePermission>();
-    
+
     private List<NodeCapability> capabilities =
-            new ArrayList<NodeConfiguration.NodeCapability>(); 
-    
+            new ArrayList<NodeConfiguration.NodeCapability>();
+
     public NodeConfiguration() {
-        
+
     }
-    
+
     public NodeConfiguration(DiscoveredService service) {
         this();
-        
+
         HashMap<String,String> properties = service.properties;
-        
+
         if (!properties.isEmpty() && properties.containsKey(NODE_TYPE)) {
             this.masterAddress = properties.get(MASTER_ADDRESS);
             this.nodeType = properties.get(NODE_TYPE);
             this.nodePath = properties.get(NODE_PATH);
-            
+
             int index = 0;
             while (properties.containsKey(NODE_CAPABILITY + "_" + index)) {
                 String value = properties.get(NODE_CAPABILITY + "_" + index);
                 this.capabilities.add(NodeCapability.fromValue(Integer.valueOf(value)));
                 index++;
             }
-            
+
             index = 0;
             while (properties.containsKey(NODE_PERMISSION_NAME + "_" + index)) {
                 String value = properties.get(NODE_PERMISSION_NAME + "_" + index);
@@ -56,7 +69,7 @@ public class NodeConfiguration {
             }
         }
     }
-    
+
     public String getMasterAddress() {
         return this.masterAddress;
     }
@@ -64,7 +77,7 @@ public class NodeConfiguration {
     public void setMasterAddress(String masterAddress) {
         this.masterAddress = masterAddress;
     }
-    
+
     public String getNodeType() {
         return this.nodeType;
     }
@@ -96,77 +109,77 @@ public class NodeConfiguration {
     public void setCapabilities(List<NodeCapability> capabilities) {
         this.capabilities = capabilities;
     }
-    
+
     public DiscoveredService toDiscoveredService() {
         DiscoveredService result = new DiscoveredService();
-        
+
         HashMap<String, String> map = new HashMap<String, String>();
-        
+
         map.put(MASTER_ADDRESS, this.masterAddress);
         map.put(NODE_TYPE, this.nodeType);
         map.put(NODE_PATH, this.nodePath);
-        
+
         for (int i = 0; i < this.permissions.size(); i++) {
             NodePermission permission = this.permissions.get(i);
             map.put(NODE_PERMISSION_NAME + "_" + i, permission.name);
             map.put(NODE_PERMISSION_EXCLUDE + "_" + i, String.valueOf(permission.exclude));
         }
-        
+
         for (int i = 0; i < this.capabilities.size(); i++) {
             NodeCapability capability = this.capabilities.get(i);
             map.put(NODE_CAPABILITY + "_" + i, String.valueOf(capability.value));
         }
-        
+
         result.properties = map;
-        
+
         return result;
     }
 
     public static class NodePermission {
         private String name;
         private boolean exclude;
-        
+
         public String getName() {
             return name;
         }
-        
+
         public void setName(String name) {
             this.name = name;
         }
-        
+
         public boolean isExclude() {
             return exclude;
         }
-        
+
         public void setExclude(boolean exclude) {
             this.exclude = exclude;
         }
     }
-    
+
     public static enum NodeCapability {
         ALL(0),
         NODE(1);
-        
+
         private final int value;
-        
+
         private NodeCapability(int value) {
             this.value = value;
         }
-        
+
         public int getValue() {
             return this.value;
         }
-        
+
         public static NodeCapability fromValue(int value) {
             NodeCapability result = null;
-            
+
             for (NodeCapability capability : NodeCapability.values()) {
                 if (capability.value == value) {
                     result = capability;
                     break;
                 }
             }
-            
+
             return result;
         }
     }
